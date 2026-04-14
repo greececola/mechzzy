d8f44d7896ad35640908756a4b9cab81754bce41
greececola <greeshmakola65@gmail.com>
2026-04-14 18:10:49 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Firebase App
+var firebaseConfig = {
+apiKey: "AIzaSyBRb3bcxCdIlOUA74TNw1hdrxfzGv-67DU",
+authDomain: "mechzzy.firebaseapp.com",
+databaseURL: "https://mechzzy-default-rtdb.asia-southeast1.firebasedatabase.app",
+projectId: "mechzzy"
+};
+
+// Initialize
+firebase.initializeApp(firebaseConfig);
+var db = firebase.database();
